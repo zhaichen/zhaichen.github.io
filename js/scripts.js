a614d1c1eecb8f2a1ecce187821953f092758031
zhaichen <chen.zhai@ptmind.com>
2020-10-28 21:19:50 +0800

    add js

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@
     "use strict"; // Start of use strict
   
     // Smooth scrolling using jQuery easing
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    $('a.js2-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
       if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
         var target = $(this.hash);
         target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -31,7 +31,7 @@
     });
   
     // Closes responsive menu when a scroll trigger link is clicked
-    $('.js-scroll-trigger').click(function() {
+    $('.js2-scroll-trigger').click(function() {
       $('.navbar-collapse').collapse('hide');
     });
   
@@ -66,4 +66,3 @@
     });
   
   })(jQuery); // End of use strict
-  
